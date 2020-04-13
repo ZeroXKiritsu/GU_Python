4ac2bcfe75a052a49d09d58b23fba0663ab62e51
ZeroXKiritsu <kalen795@gmail.com>
2020-04-13 23:23:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,138 @@
-import json
+from time import sleep
 # 1
-with open('test.txt', 'w') as my_file:
-    line = input('Введите текст \n')
-    my_file.writelines(line)
-    if not line:
-        break
-    content = my_file.readlines()
-    print(content)
-    
+class TrafficLight:
+    __color = ["красный", "желтый", "зеленый"]
+
+    def running(self):
+        i = 0
+        while i != 3:
+            print(TrafficLight.__color[i])
+        if i == 0:
+            sleep(7)
+        elif i == 1:
+            sleep(2)
+        elif i == 2:
+            sleep(5)
+        i += 1
+
+
+trafic = TrafficLight()
+trafic.running()
 # 2
-with open('test.txt', 'r') as my_file:
-    for i in my_file.readlines():
-        print(f'Количестов строк {i}')
-     for j in my_file.read():
-        print(f'Количестов слов {j}')
+class Road:
+    def __init__(self,length,width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 5
 
+    def mass(self):
+        asphalt = self._length * self._width * self.weight * self.height / 1000
+        print(f"Для покрытия всего дорожного полотна необходимо {round(asphalt)} массы асфальта")
+
+
+road = Road(5000,20)
+road.mass()
 # 3 
- with open('test.txt', 'r') as my_file:
-   oklad = []
-   sred = []
-   my_list = my_file.read().split('\n')
-   for i in my_list:
-         i = i.split()
-            if int(i[1]) < 20000:
-                sred.append(i[0])
-     oklad.append(i[1])
-print(f'Оклад меньше 20.000 {sred}, средний оклад {sum(map(int, oklad)) / len(oklad)}')
+class Worker:
+    def __init__(self, name, last_name, position, wage, bonus):
+        self.name = name
+        self.last_name = last_name
+        self.position = position
+        self._income = {"wage": int(wage), "bonus": int(bonus)}
 
+
+class Postion(Worker):
+    def __init__(self, name, last_name, position, wage, bonus):
+        super().__init__(name, last_name, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.last_name
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+pos = Postion('Kirill', 'Medved', 'QA', 100, 200)
+print(pos.get_full_name(), pos.get_total_income())
 # 4 
-my_dict = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
-new_file = []
-with open('test.txt', 'r') as my_file:
-     for i in my_file:
-        i = i.split(' ', 1)
-        new_file.append(my_dict[i[0]] + '  ' + i[1])
-    print(new_file)
- 
-with open('test_2.txt', 'w') as my_file:
-    my_file.writelines(new_file)
+class Car:
+    def __init__(self,name,speed,color,is_police = False):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
 
+    def go(self):
+        return  f"The {self.name} went."
+
+    def stop(self):
+        return f"\n The {self.name} has stoped"
+
+    def turn(self,direction):
+        return  f"\n The {self.name}  turned {direction}"
+
+    def show_speed(self):
+        return f"\n You speed is {self.speed}"
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f"\n Your speed is higher than allow! Your speed is {self.speed}"
+        else:
+            return  f"Speed of {self.name} is normal"
+
+class SportCar(Car):
+    pass
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f"\n Your speed is higher than allow! Your speed is {self.speed}"
+        else:
+            return  f"Speed of {self.name} is normal"
+
+class PoliceCar(Car):
+    pass
+
+town = TownCar('Audi', 70, 'blue', False)
+print('1:\n' + town.go(), town.show_speed(), town.turn('left'), town.turn('right'), town.stop())
+
+sport = SportCar('AudiRS', 170, 'red', False)
+print('2:\n' + sport.go(), sport.show_speed(), sport.turn('left'), sport.stop())
+
+work = WorkCar('WAZ', 30, 'red', False)
+print('3:\n' + work.go(), work.show_speed(), work.turn('right'), work.stop())
+
+police = PoliceCar('Kia', 100, 'yellow', True)
+print('4:\n' + work.go(), work.show_speed(), work.turn('right'), work.stop())
 # 5
-def summary():
-    try:
-        with open('test.txt', 'w+') as my_file:
-            line = input('Введите цифры через пробел \n')
-            my_file.writelines(line)
-            my_number = line.split()
+class Stationery:
+    def __init__(self, title):
+        self.title = title
 
-            print(sum(map(int, my_number)))
-    except IOError:
-        print('Ошибка в файле')
-    except ValueError:
-        print('Неправильно набран номер. Ошибка ввода-вывода')
-summary()
+    def draw(self):
+        return f'Запуск отрисовки'
 
-# 6 
-sub = {}
-with open('test.txt', 'r') as my_file:
-    for line in my_file:
-        subject, lecture, practice, lab = line.split()
-        sub[subject] = int(lecture) + int(practice) + int(lab)
-    print(f'Общее количество часов по предмету - \n {sub}')
-    
- # 7 
-profit = {}
-j = {}
-proove = 0
-average = 0
-i = 0
-with open('test.txt', 'r') as my_file:
-    for line in my_file:
-        name, firm, earning, damage = line.split()
-        profit[name] = int(earning) - int(damage)
-        if profit.setdefault(name) >= 0:
-            proove = proove + profit.setdefault(name)
-            i += 1
-    if i != 0:
-        average = proove / i
-        print(f'Прибыль средняя - {average:.2f}')
-    else:
-        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
-    j = {'средняя прибыль': round(average)}
-    profit.update(j)
-    print(f'Прибыль каждой компании - {profit}')
 
-with open('test.json', 'w') as my_js:
-    json.dump(profit, my_js)
+class Pen(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
 
-    js_str = json.dumps(profit)
-    print(f'Создан файл с расширением json со следующим содержимым: \n '
-          f' {js_str}') 
+
+class Pencil(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Handle(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+pen = Pen('ручкой')
+print(pen.draw())
+pencil = Pencil('карандашем')
+print(pencil.draw())
+handle = Handle('маркером')
+print(handle.draw())
