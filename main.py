2827bc0b6d74beaec762baf6e0052a127edc3106
ZeroXKiritsu <kalen795@gmail.com>
2020-04-24 21:54:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,178 @@
-from abc import ABC, abstractmethod
 # 1
-class Matrix:
-     def __init__(self, my_list):
-        self.my_list = my_list
-        
-     def __str__(self):
-        return '\n'.join(map(str, self.my_list))
-    
-      def __add__(self, other):
-        for i in range(len(self.my_list)):
-            for i_2 in range(len(other.my_list[i])):
-                self.my_list[i][i_2] = self.my_list[i][i_2] + other.my_list[i][i_2]
-        return Matrix.__str__(self)
-    
-   
-m = Matrix([[-1, 0, 1], [-1, 0, 1], [0, 1, -1], [1, 1, -1]])
-new_m = Matrix([[-2, 0, 2], [-2, 0, 2], [0, 2, -2], [2, 2, -7]])
-print(m + new_m)
+class Data:
+    def __init__(self, day_month_year):
+        # self.day = day
+        # self.month = month
+        # self.year = year
+        self.day_month_year = str(day_month_year)
 
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2019 >= year >= 0:
+                    return f'All right'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('11 - 1 - 2001')
+print(today)
+print(Data.valid(11, 11, 2022))
+print(today.valid(11, 13, 2011))
+print(Data.extract('11 - 11 - 2011'))
+print(today.extract('11 - 11 - 2020'))
+print(Data.valid(1, 11, 2000))
 # 2
-class Clothes(ABC):
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
 
-    def __init__(self, param):
-        self.param = param
-
-    @property
-    def consumption(self):
-        return f'Сумма затраченной ткани равна: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
-
-    @abstractmethod
-    def abstract(self):
-        return 'Smth vary abstract'
-
-
-class Coat(Clothes):
-    def consumption(self):
-        return f'Для пошива пальто нужно: {self.param / 6.5 + 0.5 :.2f} ткани'
-
-    def abstract(self):
-        return 'Smth vary abstract second'
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Деление на ноль недопустимо")
 
 
-class Costume(Clothes):
-    def consumption(self):
-        return f'Для пошива костюма нужно: {2 * self.param + 0.3 :.2f} ткани'
-
-    def abstract(self):
-        pass
-
-
-coat = Coat(400)
-costume = Costume(55)
-print(coat.consumption)
-print(costume.consumption())
-print(coat.abstract())
+div = DivisionByNull(10, 100)
+print(DivisionByNull.divide_by_null(10, 0))
+print(DivisionByNull.divide_by_null(10, 0.1))
+print(div.divide_by_null(100, 0))
 # 3
-class Cell:
-    def __init__(self, quantity):
-        self.quantity = int(quantity)
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+        # self.my_list = [int(i) for i in input('Введите значения через пробел ').split()]
+        # val = int(input('Введите значения и нажимайте Enter - '))
+        # self.my_list.append(val)
+        while True:
+            try:
+                val = int(input('Введите значения и нажимайте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение - строка и булево")
+                y_or_n = input(f'Попробовать еще раз? Y/N ')
+
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
+
+try_except = Error(1)
+print(try_except.my_input())
+# 4,5,6
+class StoreMashines:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    # @classmethod
+    # @staticmethod
+    def reception(self):
+        # print(f'Для выхода - Q, продолжение - Enter')
+        # while True:
+        try:
+            unit = input(f'Введите наименование ')
+            unit_p = int(input(f'Введите цену за ед '))
+            unit_q = int(input(f'Введите количество '))
+            unique = {'Модель устройства': unit, 'Цена за ед': unit_p, 'Количество': unit_q}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода - Q, продолжение - Enter')
+        q = input(f'---> ')
+        if q == 'Q' or q == 'q':
+            self.my_store_full.append(self.my_store)
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
+        else:
+            return StoreMashines.reception(self)
+
+
+class Printer(StoreMashines):
+    def to_print(self):
+        return f'to print smth {self.numb} times'
+
+
+class Scanner(StoreMashines):
+    def to_scan(self):
+        return f'to scan smth {self.numb} times'
+
+
+class Copier(StoreMashines):
+    def to_copier(self):
+        return f'to copier smth  {self.numb} times'
+
+
+unit_1 = Printer('hp', 2000, 5, 10)
+unit_2 = Scanner('Canon', 1200, 5, 10)
+unit_3 = Copier('Xerox', 1500, 1, 15)
+print(unit_1.reception())
+print(unit_2.reception())
+print(unit_3.reception())
+print(unit_1.to_print())
+print(unit_3.to_copier())
+# 7
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
 
     def __add__(self, other):
-        return f'Две клетки - хорошо, а одна большая - лучше! Размер клетки равен: {self.quantity + other.quantity}'
-
-    def __sub__(self, other):
-        sub = self.quantity - other.quantity
-        return f'Клеточка стала меньше, теперь она равна: {sub} клеточкам' if sub > 0 else 'Клетка исчезла :('
-
-    def __truediv__(self, other):
-        return self.quantity // other.quantity
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
 
     def __mul__(self, other):
-        return self.quantity * other.quantity
+        print(f'Произведение z1 и z2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
 
-    def make_order(self, row):
-        result = ''
-        for i in range(int(self.quantity / row)):
-            result += '*' * row + '\n'
-        result += '*' * (self.quantity % row) + '\n'
-        return result
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
 
 
-cell = Cell(24)
-cell_2 = Cell(2)
-print(cell + cell_2)
-print(cell - cell_2)
-print(cell / cell_2)
-print(cell * cell_2)
-print(cell.make_order(7))
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
+          
+     
